c6951729bb5f9c6f33b44cae03ee9074520deba9
julioflores2424 <69602823+julioflores2424@users.noreply.github.com>
2026-04-19 22:33:27 -0700

    Create data_memory.v

---
+++ Memory And Writeback/data_memory.v
@@ -0,0 +1,37 @@
+`timescale 1ns / 1ps
+
+module data_memory(
+    input wire clk,
+    input wire [31:0] addr,
+    input wire [31:0] write_data,
+    input wire memread, 
+    input wire memwrite,
+    output reg [31:0] read_data
+);
+
+    reg [31:0] DMEM[0:255];
+    integer i;
+
+    initial begin
+        read_data = 32'b0;
+        $readmemb("data.txt", DMEM);
+
+        for (i = 0; i < 6; i = i + 1)
+            $display("\tDMEM[%0d] = %0b", i, DMEM[i]);
+    end
+
+    // asynchronous read
+    always @(*) begin
+        if (memread)
+            read_data = DMEM[addr[31:2]];
+        else
+            read_data = 32'b0;
+    end
+
+    // synchronous write
+    always @(posedge clk) begin
+        if (memwrite)
+            DMEM[addr[31:2]] <= write_data;
+    end
+
+endmodule
